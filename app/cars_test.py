06fc0f277490bd802c309dbddaa2a4b366c8a929
buguno <brunnodesouzabezerra@gmail.com>
2022-11-11 14:37:44 -0300

    test: creating get all cars

--- app/cars_test.py
+++ app/cars_test.py
@@ -20,3 +20,13 @@ def test_should_return_correct_response_on_success_add_car():
     assert result.brand == params.brand
     assert result.model == params.model
     assert result.plate == params.plate
+
+
+def test_should_return_correct_response_on_success_get_all_cars():
+    sut.reset_cars()
+    params = make_params()
+    sut.add_car(params)
+    result = sut.get_all_cars()
+    assert result.cars[0].brand == params.brand
+    assert result.cars[0].model == params.model
+    assert result.cars[0].plate == params.plate
