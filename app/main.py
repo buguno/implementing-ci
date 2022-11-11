7d22bc783f1f1e8ef3616743d282e1da398aefcc
buguno <brunnodesouzabezerra@gmail.com>
2022-11-11 14:34:32 -0300

    feat: adding get all cars route

--- app/main.py
+++ app/main.py
@@ -1,21 +1,45 @@
 from http import HTTPStatus
+
 from fastapi import FastAPI
 
-from app.cars import CarParams, CarResponse, CarService
-
+from app.cars import CarParams, CarResponse, CarService, GetAllCars
 
 app = FastAPI()
 
 car_service = CarService()
 
+
 @app.post(
     '/car',
     responses={
-        HTTPStatus.CREATED.value: {'model': CarResponse, 'description': 'car added'},
-        HTTPStatus.INTERNAL_SERVER_ERROR.value: {'description': 'Internal Server Error'}
+        HTTPStatus.CREATED.value: {
+            'model': CarResponse,
+            'description': 'car added'
+        },
+        HTTPStatus.INTERNAL_SERVER_ERROR.value: {
+            'description': 'Internal Server Error'
+        }
     },
     status_code=HTTPStatus.CREATED,
     tags=['Cars']
 )
 def add_car(body: CarParams):
     return car_service.add_car(body)
+
+
+@app.get(
+    '/all-cars',
+    responses={
+        HTTPStatus.OK.value: {
+            'model': GetAllCars,
+            'description': 'Ok'
+        },
+        HTTPStatus.INTERNAL_SERVER_ERROR.value: {
+            'description': 'Internal Server Error'
+        }
+    },
+    status_code=HTTPStatus.OK,
+    tags=['Cars']
+)
+def get_all_cars():
+    return car_service.get_all_cars()
